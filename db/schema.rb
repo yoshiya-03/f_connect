136d44b47f5e6ef60a01f287342cedd09f3a76a3
yoshiya-03 <yoshiya03y07@gmail.com>
2021-02-16 11:00:48 +0000

    [Fix]Userモデルjob_category_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_100251) do
+ActiveRecord::Schema.define(version: 2021_02_16_105504) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
