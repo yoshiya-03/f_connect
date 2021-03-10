13acfd94671f568a605f88cbc02e3d80353f0805
yoshiya-03 <yoshiya03y07@gmail.com>
2021-03-10 06:20:27 +0000

    [Fix]クリエイター詳細ページを修正

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,19 +1,19 @@
 class CreatorsController < ApplicationController
 
   def designer
-    @users = User.where(job_category_id: 2)
-  end
-
-  def stylist
-    @users = User.where(job_category_id: 3)
-  end
-
-  def hairmake
     @users = User.where(job_category_id: 4)
   end
 
-  def photographer
+  def stylist
     @users = User.where(job_category_id: 5)
+  end
+
+  def hairmake
+    @users = User.where(job_category_id: 6)
+  end
+
+  def photographer
+    @users = User.where(job_category_id: 7)
   end
 
   private
